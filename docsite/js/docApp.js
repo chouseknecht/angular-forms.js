f31e5964b10f9e9d9cd4c9fb9dbb42a7939514dc
Chris Houseknecht <chouseknecht@me.com>
2014-01-03 01:42:22 -0500

    Fixed Download link to point to .zip file.

--- docsite/js/docApp.js
+++ docsite/js/docApp.js
@@ -15,14 +15,32 @@ var app = angular.module('docApp', ['RestService', 'Utilities']);
 app.controller('docController', ['$scope', 'Rest', 'Error',
     function($scope, Rest, Error) {
         
-        //Retrieve the current version
-        var url = '/repos/:user/:repo/tags';
+        // Get version info and download URL
+        if ($scope.removeRepoLoad) {
+            $scope.removeRepoLoad();
+        }
+        $scope.removeRepoLoad = $scope.$on('RepoLoaded', function(e, html_url) {
+            //Retrieve the current version
+            var url = '/repos/:user/:repo/releases';
+            Rest({ method: 'GET', url: url })
+                .success( function(data, status, headers, config) {
+                    $scope.version = data[0].tag_name;
+                    $scope.version_name = data[0].name; 
+                    $scope.download_url = html_url + '/releases/download/' + data[0].tag_name + '/' + data[0].assets[0].name;
+                    })
+                .error( function(data, status, headers, config) {
+                    Error({ scope: $scope, msg: 'GET ' + url + ' returned: ' + status });
+                    });
+            });
+        
+        // Get repo info
+        var url = '/repos/:user/:repo'; 
         Rest({ method: 'GET', url: url })
             .success( function(data, status, headers, config) {
-                $scope.version = data[0].name;
+                $scope.$emit('RepoLoaded', data.html_url);
                 })
             .error( function(data, status, headers, config) {
                 Error({ scope: $scope, msg: 'GET ' + url + ' returned: ' + status });
                 });
-    
+
         }]);
